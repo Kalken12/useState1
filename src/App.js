8853fbd05e6b4e06d854d66c300d7214a86f0506
Kalyani Kenekar <82832370+Kalken12@users.noreply.github.com>
2022-05-26 23:25:33 +0530

    used jsonserver post and get data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react"
+import Name from "./Components/Name";
+
 import './App.css';
 
 function App() {
+  // fetch("http://localhost:3004/todo")
+  //   .then((r) => r.json())
+  //     .then((d) => {
+
+  //       console.log(d)
+        
+  //     })
+  //     .catch((err)=>
+  //     {
+  //       console.log(err)
+  //     })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    
+      <div className="App" >
+      <Name/>
+      </div>
+   
   );
 }
 
